363c9e5961742520fc5bf2436beaf1c767a286e6
DBojneagu <93039914+DBojneagu@users.noreply.github.com>
2022-03-11 18:40:03 +0200

    update

--- POO-Project1/main.cpp
+++ POO-Project1/main.cpp
@@ -2,36 +2,50 @@
 
 using namespace std;
 
-class Carte {
+class Student {
+      string nume ;
+      int an_nastere ;
+      int numar_credite ;
+      double media_generala ;
+      Student();
+      ~Student();
+  public :
+    friend istream& operator>>(istream &i, Student&s);
+    friend ostream& operator<<(ostream &o, Student&s);
+    void afis_nume();
+    void afis_an_nastere();
+    void afis_numar_credite();
+    void afis_media_generala();
+    void afis_student();
 
-   string titlu;
-   string autor_p;
-   string autor_s;
-   int nr_pag;
-   int pret;
-   int rating;
-
-
- public:
-      void citire();
-      void afisare();
 
 };
 
-int main()
- {
-     {
+class Grupa {
 
+    int nr_studenti;
+    double media_grupei ;
+    Grupa();
+    ~Grupa();
+   public :
 
-   Carte a;
-   a.citire() ;
-   a.afisare() ;
+    void elim_student();
+    void add_student();
+    void check_by_name();
 
+};
 
-     }
+int main() {
 
-   return 0;
+}
 
- }
-void Carte::citire(){cin >> titlu >> autor_p >> autor_s >> nr_pag >> pret >> rating;}
-void Carte::afisare(){cout << " Titlul cartii este : " << titlu << " scrisa de " << autor_p ;}
+istream& operator>>(istream &i, Student&s)
+{
+    i >> s.nume >> s.an_nastere >> s.numar_credite >> s.media_generala ;
+    return i;
+}
+
+ostream& operator<<(ostream &o, Student&s)
+{
+    o << s.nume << s.an_nastere << s.numar_credite << s.media_generala ;
+}
