94cea30b82a1dfc25f7ce720da12dd0329ce1dc2
DBojneagu <david.bojneagu@yahoo.com>
2022-03-16 11:54:08 +0200

    update

--- POO-Project1/main.cpp
+++ POO-Project1/main.cpp
@@ -5,16 +5,16 @@ using namespace std;
 class Joc {
   string nume;
   int an_aparitie;
-  int numar_credite;
-  double media_generala;
+  int ore_totale_de_joc;
+  double rating;
 
   public:
 
     Joc() {
       nume = "Anonim";
       an_aparitie = 2022;
-      numar_credite = 0;
-      media_generala = 0.5;
+      ore_totale_de_joc = 0;
+      rating = 0.5;
     };
   ~Joc() {
     delete this;
@@ -22,40 +22,26 @@ class Joc {
   Joc & operator = (Joc & j);
 
   friend istream & operator >> (istream & in , Joc & j) {
-    cout << "Supraincarcare citire " << endl; in >> j.nume >> j.an_aparitie >> j.numar_credite >> j.media_generala;
+    cout << "Supraincarcare citire " << endl; in >> j.nume >> j.an_aparitie >> j.ore_totale_de_joc >> j.rating;
     cout << endl;
 
     return in;
   }
   friend ostream & operator << (ostream & out,
     const Joc & j) {
-    out << "Supraincarcare afisare" << endl << "Nume : " << j.nume << endl << "An Aparitie : " << j.an_aparitie << endl << "Numar credite : " << j.numar_credite << endl << "Media Generala: " << j.media_generala << endl << endl;
+    out << "Supraincarcare afisare" << endl << "Nume : " << j.nume << endl << "An Aparitie : " << j.an_aparitie << endl << "Numar ore de joc : " << j.ore_totale_de_joc << endl << "rating : " << j.rating << endl << endl;
     return out;
   }
 
   void set_nume(Joc & j);
   void set_an_aparitie(Joc & j);
-  void set_numar_credite(Joc & j);
-  void set_media_generala(Joc & j);
+  void set_ore_totale_de_joc(Joc & j);
+  void set_rating(Joc & j);
 
   void afis_nume(Joc & j);
   void afis_an_aparitie(Joc & j);
-  void afis_numar_credite(Joc & j);
-  void afis_media_generala(Joc & j);
-
-};
-
-class Grupa {
-
-  int nr_studenti;
-  double media_grupei;
-  Grupa();
-  ~Grupa();
-  public:
-
-  void elim_student();
-  void add_student();
-  void check_by_name();
+  void afis_ore_totale_de_joc(Joc & j);
+  void afis_rating(Joc & j);
 
 };
 
@@ -76,9 +62,9 @@ int main() {
   B.set_an_aparitie(B);
   B.afis_an_aparitie(B);
 
-  B.afis_numar_credite(B);
+  B.afis_ore_totale_de_joc(B);
 
-  B.afis_media_generala(B);
+  B.afis_rating(B);
 
   return 0;
 }
@@ -89,11 +75,11 @@ void Joc::afis_nume(Joc & j) {
 void Joc::afis_an_aparitie(Joc & j) {
   cout << "An Aparitie: " << j.an_aparitie << endl;
 }
-void Joc::afis_numar_credite(Joc & j) {
-  cout << "Numar credite: " << j.numar_credite << endl;
+void Joc::afis_ore_totale_de_joc(Joc & j) {
+  cout << "Numar ore totale de joc : " << j.ore_totale_de_joc << endl;
 }
-void Joc::afis_media_generala(Joc & j) {
-  cout << "Media Generala: " << j.media_generala << endl;
+void Joc::afis_rating(Joc & j) {
+  cout << "rating :  " << j.rating << endl;
 }
 
 void Joc::set_nume(Joc & j) {
@@ -104,13 +90,13 @@ void Joc::set_an_aparitie(Joc & j) {
   cout << "An Aparitie " << endl;
   cin >> j.an_aparitie;
 }
-void Joc::set_numar_credite(Joc & j) {
-  cout << "Numar credite: " << endl;
-  cin >> j.numar_credite;
+void Joc::set_ore_totale_de_joc(Joc & j) {
+  cout << "Numar ore totale de joc: " << endl;
+  cin >> j.ore_totale_de_joc;
 }
-void Joc::set_media_generala(Joc & j) {
-  cout << "Medie Generala " << endl;
-  cin >> j.media_generala;
+void Joc::set_rating(Joc & j) {
+  cout << "rating " << endl;
+  cin >> j.rating;
 }
 
 Joc & Joc::operator = (Joc & j) {
@@ -118,8 +104,8 @@ Joc & Joc::operator = (Joc & j) {
     cout << "operator de atribuire explicit: ";
     nume = j.nume;
     an_aparitie = j.an_aparitie;
-    numar_credite = j.numar_credite;
-    media_generala = j.media_generala;
+    ore_totale_de_joc = j.ore_totale_de_joc;
+    rating = j.rating;
 
   }
   return *this;
